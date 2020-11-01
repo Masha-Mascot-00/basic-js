b2d0875bf6f7499721b8d355c91ef54eb1f33a15
Masha-Mascot-00 <mashamascot969@gmail.com>
2020-11-01 17:28:18 +0300

    feat: add solution for 'transform array' task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
+  calculateDepth(/* array */) {
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  // remove line with error and write your code here
+    /* let current = this;
+    let depth = current.depth;
+    let maxDepth = 1;
+
+    if (current.depth === undefined) {
+      current.depth = 0;
+    }
+    array.forEach((element) => {
+      if (Array.isArray(element)) {
+        current.depth = current.depth + 1;
+        current.calculateDepth(element);
+      } else {
+        depth = current.depth;
+        if (depth > maxDepth) {
+          maxDepth = depth;
+        } else {
+          depth = maxDepth;
+        }
+      }
+    });
+    //depth = current.depth;
+    return maxDepth; */
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) {
+    throw new Error("Error");
+  }
+
+  let transformedArray;
+  transformedArray = arr.slice(0, arr.length);
+
+  for (let index = 0; index < transformedArray.length; index++) {
+    let element = transformedArray[index];
+    switch (element) {
+      case "--discard-next":
+        if (index + 1 < transformedArray.length) {
+          transformedArray[index + 1] = null;
+        }
+
+        transformedArray[index] = null;
+        break;
+      case "--discard-prev":
+        if (index - 1 >= 0) {
+          transformedArray[index - 1] = null;
+        }
+
+        transformedArray[index] = null;
+        break;
+      case "--double-next":
+        if (index + 1 < transformedArray.length) {
+          transformedArray[index] = transformedArray[index + 1];
+          index++;
+        } else {
+          transformedArray[index] = null;
+        }
+        break;
+      case "--double-prev":
+        if (index - 1 >= 0) {
+          transformedArray[index] = transformedArray[index - 1];
+          index++;
+        } else {
+          transformedArray[index] = null;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  let resultArray = [];
+  transformedArray.forEach((element) => {
+    if (element !== null) {
+      resultArray.push(element);
+    }
+  });
+  return resultArray;
 };
